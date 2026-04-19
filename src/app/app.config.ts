ed599abc58538bdd305963ec1598117d01104e51
saif-alshaiban-AMIC <salshaiban@alkhorayef.com>
2026-04-19 09:30:54 +0300

    s

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
-
-
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 import { CustomTranslateLoader } from './i18n.loader';
 
-
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    // provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
 
     provideHttpClient(withInterceptors([authInterceptor])),
